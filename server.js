dab5ab2096a34de43eee5c4265f69087b12bee14
PatriciaFeio <patriciafeio@gmail.com>
2023-06-13 15:33:01 +0100

    feat: get and post routes and added dotenv to use costum variables

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-// Setup empty JS object to act as endpoint for all routes
+// Empty JS object to act as endpoint for all routes
 projectData = {};
 
-/* Dependencies */
+// dotenv to use custom environment variables
+require('dotenv').config();
+
+// Express to run server and routes
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 // Start up an instance of app
 const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -20,12 +24,43 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const key = process.env.API_KEY
 
-// Setup Server
-const port = 3000;
-
+// Spin up the server
+const port = process.env.PORT || 3000;
 const server = app.listen(port, listening);
 
 function listening() {
-    console.log(`running on localhost: ${port}`);
+    console.log(`Running on localhost: ${port}`);
 };
+
+// GET route for projectData
+app.get('/all', getData);
+
+function getData (req, res) {
+    res.send(projectData);
+};
+
+// GET route for API Key
+app.get('/key', getKey);
+
+function getKey(req, res) {
+    res.send(key);
+}
+
+// POST route
+app.post('/add', postData);
+
+function postData (req, res) {
+    projectData = req.body;
+    projectData['date'] = req.body.date;
+    projectData['city'] = req.body.city;
+    projectData['icon'] = req.body.icon;
+    projectData['temp'] = req.body.temp;
+    projectData['description'] = req.body.description;
+    projectData['feelings'] = req.body.feelings;
+    projectData['hour'] = req.body.hour;
+    res.send(projectData);
+};
+
+
